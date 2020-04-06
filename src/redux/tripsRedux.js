2960714c5295a850e664480d82c1dab9e57f2767
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-04-06 12:22:30 +0200

    Add filters for tripId and countryCode

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -13,17 +13,24 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
+  if(filters.duration){
+    const pattern = new RegExp(filters.duration, 'i');
+    output = output.filter(trip => pattern.test(trip.duration));
+  }
 
   // TODO - filter by tags
-
+  if(filters.tags){
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.tags));
+  }
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
 
+  const filtered = trips.filter(trip => trip.id == tripId);
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
@@ -31,8 +38,8 @@ export const getTripById = ({trips}, tripId) => {
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
 
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
   // TODO - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
